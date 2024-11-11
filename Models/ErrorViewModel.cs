d5f5ba0acf91c294db97f502e1c5860854356c2d
TANA-PC\JSrad <zhitai12233@gmail.com>
2024-11-11 10:54:42 +0800

    Revert "CoffeeMapVer--地圖頁面專區初版(未搬移完成)20241111"
    This reverts commit 5a007eecf19d3f54efbea14f3fa94d03e649e1ff.

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CoffeeMap20241106.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
