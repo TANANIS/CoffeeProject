b8d121c04fef468e8034f68b44922b8583df4603
TANANIS <zhitai0112@gmail.com>
2024-11-21 11:04:32 +0800

    20241121清除舊模板(重新整合版本上傳)

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Coffee.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
